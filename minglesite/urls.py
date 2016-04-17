53ac4250b57dfa1c06d0d7f6dd21383e4f6a237a
Mingle Chang <mingle890805@gmail.com>
2016-04-17 21:36:54 +0800

    not

--- minglesite/urls.py
+++ minglesite/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^', include('blog.urls')),
+    url(r'^', include('blog.urls',namespace="blog")),
     
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls',namespace="polls")),
 ]
