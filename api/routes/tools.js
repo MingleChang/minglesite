5b736614aa89d87bdf8e0bf0df52f57b02c805a1
Mingle Chang <mingle890805@gmail.com>
2017-04-09 23:18:22 +0800

    datetime

--- api/routes/tools.js
+++ api/routes/tools.js
@@ -23,4 +23,11 @@ router.get('/ip', function(req, res, next) {
 	});
 });
 
+router.get('/datetime', function(req, res, next) {
+	let timestamp = Date.now();
+	let jsonInfo = {"timestamp":timestamp};
+	let info = global.funcs.jsonInfo(200, "成功", jsonInfo);
+	res.send(info);
+});
+
 module.exports = router;
