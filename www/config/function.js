9ea978adaf1dfda81e915eb340a9d2b949440f84
Mingle Chang <mingle890805@gmail.com>
2017-04-09 18:24:14 +0800

    not

--- www/config/function.js
+++ www/config/function.js
@@ -1,11 +1,46 @@
 const funcs = {
+	//跳转404页面
     page404: function(req, res, next) {
     	res.render('404');
     },
-
+    //跳转502页面
     page502: function(err, req, res, next) {
     	res.render('502');
-    }
+    },
+    //将plist字符串转换为json对象
+    plist2json: function(plist, callback) {
+    	const xml2js = require('xml2js');
+		let startIndex = plist.indexOf("<?xml");
+		let endIndex = plist.indexOf("</plist>") + 8;
+		let xml = plist.substring(startIndex, endIndex);
+		xml2js.parseString(xml, function(err, result){
+			let keys = result.plist.dict[0].key;
+			let strings = result.plist.dict[0].string;
+			let json = {};
+			for (let i = 0; i < keys.length; i++) {
+				json[keys[i]] = strings[i];
+			};
+			callback(json);
+		})
+	},
+	//用json对象生成get请求的参数部分
+	json2getparam: function(json) {
+		let param = "";
+		for (let key in json) {
+			if (param == "") {
+				param = param + '?' + key + '=' + json[key];
+			} else {
+				param = param + '&' + key + '=' + json[key];
+			}
+		}
+		return param;
+	},
+	//用plist信息生成get请求的参数部分
+	plist2getparam: function(plist, callback) {
+		global.funcs.plist2json(plist, function(json){
+			callback(global.funcs.json2getparam(json))
+		})
+	}
 };
 
 module.exports = funcs;
--- www/routes/udid.js
+++ www/routes/udid.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
-const router = express.Router()
+const xml2js = require('xml2js');
+const router = express.Router();
 
 router.get('/', function(req, res, next) {
 	res.render('udid'); 
@@ -12,20 +13,20 @@ router.get('/get', function(req, res, next) {
 })
 
 router.post('/receive', function(req, res, next) {
-	console.log(req.body);
-	var param = "";
-	for (var key in req.query) {
-		if (param == "") {
-			param = '?' + key + '=' + req.query[key];
-		} else {
-			param = param + '&' + key + '=' + req.query[key];
-		}
-	}
-	res.redirect(301, '/udid/result' + param);
+	let plist = '';//添加接收变量
+	req.setEncoding('utf8');
+	req.on('data', function(chunk) { 
+		plist += chunk;
+	});
+	req.on('end', function() {
+		global.funcs.plist2getparam(plist, function(param) {
+			res.redirect(301, '/udid/result' + param);
+		});
+	});
 })
 
 router.get('/result', function(req, res, next) {
-	res.render('result');
+	res.render('result', req.query);
 })
 
 module.exports = router
