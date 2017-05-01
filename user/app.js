e1c4aedfc2ecee4f688824b19504b5e1b41bbb30
Mingle Chang <mingle890805@gmail.com>
2017-05-01 22:35:49 +0800

    not

---
+++ user/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const multer  = require('multer');
+const path = require('path');
+
+const account = require('./routes/account');
+const user = require('./routes/user');
+
+//定义全局变量
+global.config = require('./config/config');
+//定义公共函数
+global.funcs = require('./config/function');
+
+let app = express();
+
+app.set('trust proxy', 'loopback');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(session({
+  secret: global.config.session_secret,
+  cookie: { path: '/', httpOnly: true, maxAge: 60 * 1000 * 30} ,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use('/account', account);
+app.use('/user', user);
+
+//404
+app.use(global.funcs.json404);
+//502
+app.use(global.funcs.json502);
+
+module.exports = app;
---
+++ user/config/config.js
@@ -0,0 +1,17 @@
+const config = {
+	name: 'user',
+	description: 'user description',
+	debug: false,
+	session_secret: 'minglechang',
+
+	//端口
+	port: 8002,
+
+	//MySQL
+	db_host: 'localhost',
+	db_database: 'mingle',
+	db_user: 'root',
+	db_password: '890805'
+};
+
+module.exports = config;
