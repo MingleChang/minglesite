b3d2688b7cb4dc2ca77e32b2c08327aeb6a3adf9
Mingle Chang <mingle890805@gmail.com>
2017-06-29 23:01:42 +0800

    change bug

---
+++ www/statics/js/udidResult.js
@@ -0,0 +1,11 @@
+var app=angular.module("udidResult",[]);
+app.config(['$locationProvider', function($locationProvider) {  
+    $locationProvider.html5Mode({
+      	enabled: true,
+      	requireBase: false
+    });
+}]);
+app.controller('udidResult', function($scope, $location) {
+    $scope.device =$location.search().PRODUCT;
+    $scope.udid =$location.search().UDID;
+}); 
