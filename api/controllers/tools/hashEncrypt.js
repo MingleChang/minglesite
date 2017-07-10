bdb5ec620ab284935c0d4ff18c0dd1aa3f28f65b
mingle <32973902@qq.com>
2017-07-10 19:33:43 +0800

    Add api

---
+++ api/controllers/tools/hashEncrypt.js
@@ -0,0 +1,36 @@
+const crypto = require('crypto');
+/*
+source:明文
+method:加密方式
+password：密码(可选,如果传入了password则认为是hmac)
+*/
+function  hashEncrypt(req, res, next) {
+	let source = req.query.source;
+	let method = req.query.method;
+	let password = req.query.password;
+	if (!source) {
+		let err = new Error('明文不能为空');
+		err.status = 400;
+		next(err);
+		return;
+	}
+	if (!method) {
+		let err = new Error('加密方式错误');
+		err.status = 400;
+		next(err);
+		return;
+	}
+	let cipher; 
+	if (password == undefined) {
+		cipher = crypto.createHash(method);
+	}else {
+		cipher = crypto.createHmac(method, password);
+	}
+	cipher.update(source);
+	let ciphertext = cipher.digest('hex');
+	let jsonInfo = {"source":source,"method":method,"password":password,"result":ciphertext};
+	let info = global.funcs.jsonInfo(200, "成功", jsonInfo);
+	res.send(info);
+}
+
+module.exports = hashEncrypt;
--- api/routes/tools.js
+++ api/routes/tools.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const getIp = require('../controllers/tools/getIp');
 const getDatetime = require('../controllers/tools/getDatetime');
+const hashEncrypt = require('../controllers/tools/hashEncrypt');
 
 router.get('/ip', function(req, res, next) {
 	getIp(req, res, next);
@@ -10,6 +11,10 @@ router.get('/ip', function(req, res, next) {
 
 router.get('/datetime', function(req, res, next) {
 	getDatetime(req, res, next);
+});
+
+router.get('/hashEncrypt', function(req, res, next) {
+	hashEncrypt(req, res, next);
 });
 
 module.exports = router;
