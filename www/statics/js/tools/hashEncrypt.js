484cf82949fa6650c0d15a5fbd78eee452bffecc
mingle <32973902@qq.com>
2017-07-11 16:27:08 +0800

    add

--- www/statics/js/tools/hashEncrypt.js
+++ www/statics/js/tools/hashEncrypt.js
@@ -15,23 +15,31 @@ app.config(['$locationProvider', function($locationProvider) {
 //     $qProvider.errorOnUnhandledRejections(false);
 // }]);
 app.controller('hashEncrypt', function($http,$scope) {
-	$scope.buttonMD5 = function () {
-		encrypt($scope.sourceText, 'md5');
-	}
-	$scope.buttonSHA1 = function () {
-		encrypt($scope.sourceText, 'sha1');
-	}
-	$scope.buttonSHA224 = function () {
-		encrypt($scope.sourceText, 'sha224');
-	}
-	$scope.buttonSHA256 = function () {
-		encrypt($scope.sourceText, 'sha256');
-	}
-	$scope.buttonSHA384 = function () {
-		encrypt($scope.sourceText, 'sha384');
-	}
-	$scope.buttonSHA512 = function () {
-		encrypt($scope.sourceText, 'sha512');
+	$scope.visible = false;
+
+	$scope.encrypts = [{'name':'MD5','method':'md5','isHmac':false},
+						{'name':'SHA1','method':'sha1','isHmac':false},
+						{'name':'SHA224','method':'sha224','isHmac':false},
+						{'name':'SHA256','method':'sha256','isHmac':false},
+						{'name':'SHA384','method':'sha384','isHmac':false},
+						{'name':'SHA512','method':'sha512','isHmac':false},
+						{'name':'HmacMD5','method':'md5','isHmac':true},
+						{'name':'HmacSHA1','method':'sha1','isHmac':true},
+						{'name':'HmacSHA224','method':'sha224','isHmac':true},
+						{'name':'HmacSHA256','method':'sha256','isHmac':true},
+						{'name':'HmacSHA384','method':'sha384','isHmac':true},
+						{'name':'HmacSHA512','method':'sha512','isHmac':true},]
+	$scope.buttonClick = function (item) {
+		$scope.visible = item.isHmac;
+		if (item.isHmac) {
+			var password = $scope.password;
+			if (!password) {
+				password = '';
+			}
+			encrypt($scope.sourceText, item.method, password);
+		}else {
+			encrypt($scope.sourceText, item.method);
+		}
 	}
 
 	function encrypt(source,method,password) {
@@ -42,7 +50,7 @@ app.controller('hashEncrypt', function($http,$scope) {
 		var url = "http://api.minglechang.com/tools/hashEncrypt";
 		url = url + '?source=' + source;
 		url = url + '&method=' + method;
-		if (password) {
+		if (password != undefined) {
 			url = url + '&password=' + password;
 		}
 		$http.get(url
